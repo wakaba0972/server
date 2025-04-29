ea64c9fd01768636724307b89bdfa1a6b8977e79
wakaba0972 <benny.wu2199@gmail.com>
2025-04-29 23:09:59 +0800

    修正 .setDescription只能有a~z, 0~9, _, -

--- commands/utility/topic.js
+++ commands/utility/topic.js
@@ -1,17 +1,17 @@
 const { SlashCommandBuilder } = require('discord.js');
-const Topic = require('.../self_modules/TopicUnit');
+//const Topic = require('../../self_modules/TopicUnit');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('topic')
-		.setDescription('利用"/topic [主題]" 生成劇本')
+		.setDescription('to generate a script')
         .addStringOption(option =>
-            option.setName('Topic')
-                .setDescription('請輸入主題')
+            option.setName('topic')
+                .setDescription('topic')
                 .setRequired(true)
         ),
 	async execute(interaction) {
-        const topic = interaction.options.getString('Topic');
+        const topic = interaction.options.getString('topic');
         const unit = new Topic("user", topic);
 
         unit.execute().then((result) => {
@@ -19,7 +19,7 @@ module.exports = {
         }
         ).catch((error) => {
             console.error('Error:', error);
-            interaction.reply('發生錯誤，請稍後再試。');
+            interaction.reply('error');
         });
     }
 };
