7446e6b6cb0c90fa4c1323748efa01e0faf29d79
wakaba0972 <benny.wu2199@gmail.com>
2025-05-02 11:29:33 +0800

    add Ttsclient.js

---
+++ self_modules/TtsClient.js
@@ -0,0 +1,70 @@
+const axios = require('axios');
+
+class ttsClient {
+    constructor(text, character, lang){
+        this.text = text;
+        this.character = character;
+        this.lang = lang;
+    }
+
+    async gpt(){
+        try {
+            const response = await axios.get('http://localhost:9880/set_gpt_weights?weights_path=GPT_weights_v2/sponge01-e15.ckpt');
+            return response.data;
+        } catch (error) {
+            console.error('Error initializing TTS:', error);
+            throw error;
+        }
+    }
+
+    async sovits(){
+        try {
+            const response = await axios.get('http://localhost:9880/set_sovits_weights?weights_path=SoVITS_weights_v2/sponge01_e8_s216.pth');
+            return response.data;
+        } catch (error) {
+            console.error('Error initializing TTS:', error);
+            throw error;
+        }
+    }
+
+    async init(){
+        await this.gpt();
+        await this.sovits();
+        return;
+    }
+
+    async tts(){
+        try {
+            const response = await axios.post('http://localhost:9880/tts', {
+                "text": this.text,
+                "text_lang": this.lang,
+                "ref_audio_path": "C:\\Users\\88690\\Desktop\\GPT-SoVITS-v4-20250422fix\\output\\denoise_opt\\We're not talking about this - Original vs MS Paint (mp3cut.net) (3).wav",
+                "aux_ref_audio_paths": [],
+                "prompt_lang": "en",
+                "prompt_text": "Then%20you%27ll%20say%20we%27re%20not%20talking%20about%20this%20or%20this%20we%27re%20talking%20about%20thissss!",
+                "top_k": 5,
+                "top_p": 1,
+                "temperature": 1,
+                "text_split_method": "cut5",
+                "batch_size": 1,
+                "batch_threshold": 0.75,
+                "split_bucket": true,
+                "speed_factor": 1,
+                "fragment_interval": 0.3,
+                "seed": -1,
+                "media_type": "wav",
+                "streaming_mode": false,
+                "parallel_infer": true,
+                "repetition_penalty": 1.35,
+                "sample_steps": 32,
+                "super_sampling": false
+            });
+            return response.data;
+        } catch (error) {
+            console.error('Error generating TTS:', error);
+            throw error;
+        }
+    }
+}
+
+module.exports = ttsClient;
