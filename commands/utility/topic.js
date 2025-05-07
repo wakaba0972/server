2d97d60c8b317269d2b7799a29f5b22365528bbd
wakaba0972 <benny.wu2199@gmail.com>
2025-05-08 03:12:58 +0800

    將groq request改為json mode

--- commands/utility/topic.js
+++ commands/utility/topic.js
@@ -16,20 +16,20 @@ module.exports = {
         ),
 	async execute(interaction) {
         const topic = interaction.options.getString('topic');
+        await interaction.deferReply();
 
         try{
             const unit = new Topic("user", topic);
-            unit.execute().then((result) => {
-                interaction.reply(result);
-            }
-            ).catch((error) => {
-                console.error('Error:', error);
-                interaction.reply('error');
+
+            let result = await unit.execute();
+            await interaction.editReply({
+                content: "\`\`\`json\n" + result + "\`\`\`"
             });
+
         } catch (error) {
             console.error('Error generating topic:', error);
-            interaction.reply({
-                content: "發生錯誤，請稍後再試。",
+            await interaction.editReply({
+                content: "發生錯誤"
             });
         }
     }
--- self_modules/GroqClient.js
+++ self_modules/GroqClient.js
@@ -6,29 +6,31 @@
 require('dotenv').config()
 const Groq = require('groq-sdk')
 
-const MODEL = 'llama3-70b-8192'
+const MODEL = 'llama-3.3-70b-versatile'
 
-const PROMPT = `
-用中文生成一個海綿寶寶的劇本，角色從［海綿寶寶、派大星、蟹老闆、章魚哥、小蝸］中挑選2到3個角色，臺詞總共約1000字，用json格式如下
-\`\`\`json
+const SYSTEM_PROMPT = `
+永遠以json格式輸出劇本
+`
+
+const USER_PROMPT = `
+用中文生成一個海綿寶寶的劇本，角色從［海綿寶寶、派大星、蟹老闆、章魚哥、小蝸］中挑選2到3個角色，臺詞總共約500字，永遠以json格式輸出劇本，不能添加其他文字，json格式如下:
 {
-  "topic": "主題名稱",
+  "topic": "打招呼",
   "script": [
     {
-      "character": "A",
+      "character": "海綿寶寶",
       "text": "嗨",
     },
     {
-      "character": "B",
+      "character": "派大星",
       "text": "嗨",
     },
     {
-      "character": "C",
+      "character": "蟹老闆",
       "text": "嗨",
     },
   ]
 }
-\`\`\`
 `
 
 const WARNING = "'''只要輸出json劇本就好，不要添加其他文字'''";
@@ -44,10 +46,11 @@ class GroqClient {
     try {
         const chatCompletion = await this.client.chat.completions.create({
             messages: [{ 
-                //role: 'system', content: prompt,
-                role: 'user', content: `${PROMPT},主題是${message},${WARNING}`,
+                role: 'system', content: SYSTEM_PROMPT,
+                role: 'user', content: `${USER_PROMPT},主題是${message},${WARNING}`,
             }],
             model: MODEL,
+            response_format: {"type": "json_object"},
         });
         
         return chatCompletion.choices[0].message.content;
