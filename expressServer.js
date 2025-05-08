23dd3cb2487c6f19a22a5678cf5006dd579976c2
wakaba0972 <benny.wu2199@gmail.com>
2025-05-09 00:14:01 +0800

    新增/scripts?id 路由

--- expressServer.js
+++ expressServer.js
@@ -3,6 +3,7 @@
   之後會用來處理Unity端的請求
 */
 
+const fs = require('fs');
 const express = require('express');
 const PORT = process.env.PORT || 3000;
 
@@ -11,9 +12,20 @@ const app = express();
 app.use(express.static('wavs'))
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+    res.send('Hello World!');
+});
+
+app.get('/scripts', (req, res) => {
+    const id = req.query.id;
+    const filePath = `./results/scripts/${id}.json`;
+
+    fs.readFile(filePath, (err, data)=> {
+        if (err) throw err;
+        res.setHeader('Content-Type', 'application/json');
+        res.send(data);
+    });
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
